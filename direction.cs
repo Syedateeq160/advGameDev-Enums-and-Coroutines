cba0c3035705583becdfd1eb4aa190608264227d
Syedateeq160 <syedateeq1000@gmail.com>
2025-05-04 14:11:12 +0300

    Adding UI button integration for direction changes

--- direction.cs
+++ direction.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-// Renamed enum from MovementDirection to NavigationDirection
 public enum NavigationDirection
 {
-    // Renamed values but kept functionality
     Upward,
     Downward,
     Leftward,
@@ -13,15 +12,64 @@ public enum NavigationDirection
 
 public class Direction : MonoBehaviour
 {
-    // Adding version tracking
     private const string VERSION = "1.0.0";
     
     public NavigationDirection _navigationDirection;
     public Player player;
     
-    // Adding debugging functionality
+    // Adding new UI button references
+    [Header("UI Controls")]
+    public Button upButton;
+    public Button downButton;
+    public Button leftButton;
+    public Button rightButton;
+    
     void Start()
     {
         Debug.Log("Direction controller initialized with version: " + VERSION);
+        
+        // Setting up button listeners
+        SetupButtonListeners();
+    }
+    
+    // Adding new method for UI setup
+    private void SetupButtonListeners()
+    {
+        if (upButton != null)
+            upButton.onClick.AddListener(() => SetDirectionUpward());
+            
+        if (downButton != null)
+            downButton.onClick.AddListener(() => SetDirectionDownward());
+            
+        if (leftButton != null)
+            leftButton.onClick.AddListener(() => SetDirectionLeftward());
+            
+        if (rightButton != null)
+            rightButton.onClick.AddListener(() => SetDirectionRightward());
+    }
+    
+    // Changing method names and implementations
+    public void SetDirectionUpward() 
+    {
+        if (player != null)
+            player.QueueDirectionChange(NavigationDirection.Upward);
+    }
+    
+    public void SetDirectionDownward() 
+    {
+        if (player != null)
+            player.QueueDirectionChange(NavigationDirection.Downward);
+    }
+    
+    public void SetDirectionLeftward() 
+    {
+        if (player != null)
+            player.QueueDirectionChange(NavigationDirection.Leftward);
+    }
+    
+    public void SetDirectionRightward() 
+    {
+        if (player != null)
+            player.QueueDirectionChange(NavigationDirection.Rightward);
     }
 }
